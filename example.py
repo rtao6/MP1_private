b03f1a830ee0dd92affd7b57cc7a15dc2b2f1f1b
Tao, Jerry <rtao6@illinois.edu>
2022-08-29 19:33:18 -0500

    finished on exmaple.py

--- example.py
+++ example.py
@@ -1,11 +1,19 @@
+from contextlib import suppress
 import metapy
 
 def tokens_lowercase(doc):
     #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"), 
     #lowercases, removes words with less than 2 and more than 5  characters
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
-    '''Place your code here'''
-    
+
+    tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+    tok = metapy.analyzers.LengthFilter(tok,min = 2,max=5) #filter all 
+    tok = metapy.analyzers.LowercaseFilter(tok) #  lowercaseall words
+    tok.set_content(doc.content())
+    ana = metapy.analyzers.NGramWordAnalyzer(3, tok)
+    trigrams = ana.analyze(doc)
+
+
     #leave the rest of the code as is
     tok.set_content(doc.content())
     tokens, counts = [], []
@@ -13,7 +21,7 @@ def tokens_lowercase(doc):
         counts.append(count)
         tokens.append(token)
     return tokens
-    
+
 if __name__ == '__main__':
     doc = metapy.index.Document()
     doc.content("I said that I can't believe that it only costs $19.95! I could only find it for more than $30 before.")
